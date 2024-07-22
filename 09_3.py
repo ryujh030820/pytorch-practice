8a9d6d4464da8f92075e0334eb7c500395b99ecb
ryujh030820 <ryujh030820@gmail.com>
2024-07-22 12:50:37 +0900

    [PyTorch] Lab-09-3 Dropout

---
+++ 09_3.py
@@ -0,0 +1,89 @@
+import torch
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import random
+import ssl
+import certifi
+import matplotlib.pyplot as plt
+
+# SSL 인증서 관련
+ssl._create_default_https_context = ssl._create_unverified_context
+ssl._create_default_https_context = ssl._create_stdlib_context
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# for reproducibility
+random.seed(777)
+torch.manual_seed(777)
+if device == 'cuda':
+    torch.cuda.manual_seed_all(777)
+
+# parameters
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+drop_prob = 0.5
+
+mnist_train = dsets.MNIST(root='MNIST_data/', train=True, transform=transforms.ToTensor(), download=True)
+
+mnist_test = dsets.MNIST(root="MNIST_data/", train=False, transform=transforms.ToTensor(), download=True)
+
+data_loader = torch.utils.data.DataLoader(dataset=mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+
+# MNIST data image of shape 28 * 28 = 784
+linear1 = torch.nn.Linear(784, 256, bias=True).to(device)
+linear2 = torch.nn.Linear(256, 256, bias=True).to(device)
+linear3 = torch.nn.Linear(256, 10, bias=True).to(device)
+relu = torch.nn.ReLU()
+dropout = torch.nn.Dropout(p=drop_prob)
+
+# Initialization
+torch.nn.init.xavier_uniform_(linear1.weight)
+torch.nn.init.xavier_uniform_(linear2.weight)
+torch.nn.init.xavier_uniform_(linear3.weight)
+
+# model
+model = torch.nn.Sequential(linear1, relu, dropout, linear2, relu, dropout, linear3).to(device)
+
+# define cost/Loss & optimizer
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+model.train()
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = len(data_loader)
+    for X, Y in data_loader:
+        # reshape input image into [batch_size by 784]
+        # label is not one-hot encoded
+        X = X.view(-1, 28 * 28).to(device)
+        optimizer.zero_grad()
+        hypothesis = model(X)
+        cost = criterion(hypothesis, Y)
+        cost.backward()
+        optimizer.step()
+        avg_cost += cost / total_batch
+
+    print("Epoch: ", "%04d" % (epoch+1), "cost =", "{:.9f}".format(avg_cost))
+
+# Test the model using test sets (grad를 변경하지 않음)
+with torch.no_grad():
+    model.eval()
+    X_test = mnist_test.data.view(-1, 28 * 28).float().to(device)
+    Y_test = mnist_test.targets.to(device)
+
+    prediction = model(X_test)
+    correct_prediction = torch.argmax(prediction, 1) == Y_test
+    accuracy = correct_prediction.float().mean()
+    print("Accuracy: ", accuracy.item())
+
+r = random.randint(0, len(mnist_test.data) - 1)
+X_single_data = mnist_test.data[r:r + 1].view(-1, 28 * 28).float().to(device)
+Y_single_data = mnist_test.targets[r:r + 1].to(device)
+
+print("Label: ", Y_single_data.item())
+single_prediction = model(X_single_data)
+print("Prediction: ", torch.argmax(single_prediction, 1).item())
+
+plt.imshow(mnist_test.data[r:r + 1].view(28, 28), cmap="Greys", interpolation="nearest")
+plt.show()
